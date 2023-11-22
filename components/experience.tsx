b948247243177d90c613e715397217d8f46b410d
Eduardo Taveras <eduardoenriquetaveras@gmail.com>
2023-11-21 19:41:09 -0500

    Comment SectionView

--- components/experience.tsx
+++ components/experience.tsx
@@ -10,12 +10,12 @@ import { useTheme } from '@/context/theme-context';
 
 
 export default function Experience() {
-  const {ref} = useSectionInView("Experience", 0.80)
+ // const {ref} = useSectionInView("Experience", 0.80)
   const {theme} = useTheme()
 
 
   return (
-    <section ref = {ref} id="experience" className='scroll-mt-28 mb-28 sm:mb-40'>
+    <section /*ref = {ref}*/ id="experience" className='scroll-mt-28 mb-28 sm:mb-40'>
         <SectionHeading>My experience</SectionHeading>
         <VerticalTimeline lineColor=''>
                 {experiencesData.map((item, index) => (
